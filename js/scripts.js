224bcdc4c0c8d2d703de16d5ed1d480e0f0c5c83
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-07-28 15:28:32 +0200

    Esercizio completato

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var nome = prompt("Inserisci il nome");
+var cognome = prompt("Inserisci il cognome");
+var colore = prompt("Inserisci il tuo colore preferito");
+document.getElementById("name").innerHTML += nome;
+document.getElementById("surname").innerHTML += cognome;
+document.getElementById("color").innerHTML += colore;  
+
+      
