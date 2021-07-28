4eaf381dd0ff11ec074e8c8f72ea1c5476f1f64b
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-07-28 15:43:28 +0200

    Esercizio corretto

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var cognome = prompt("Inserisci il cognome");
 var colore = prompt("Inserisci il tuo colore preferito");
 document.getElementById("name").innerHTML += nome;
 document.getElementById("surname").innerHTML += cognome;
-document.getElementById("color").innerHTML += colore;  
+document.getElementById("color").innerHTML += colore;
+document.getElementById("n-40").innerHTML += "40";   
 
       
