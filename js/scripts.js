eed44c1c091834a66b98166066432a23d4a5e00b
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-07-28 15:52:59 +0200

    Ottimizzazione codice

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 var nome = prompt("Inserisci il nome");
 var cognome = prompt("Inserisci il cognome");
 var colore = prompt("Inserisci il tuo colore preferito");
-document.getElementById("name").innerHTML += nome;
-document.getElementById("surname").innerHTML += cognome;
-document.getElementById("color").innerHTML += colore;
-document.getElementById("n-40").innerHTML += "40";   
-
+var n40 = "40";
+document.getElementById("info").innerHTML += nome + cognome + colore + n40;
       
